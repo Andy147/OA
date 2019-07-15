aa741f196417f818fcf04bbcf7e3e365fd089a19
andy <wlg118325338@163.com>
2019-07-15 22:47:09 +0800

    修改测试

--- src/main/java/org/oa/controller/UserController.java
+++ src/main/java/org/oa/controller/UserController.java
@@ -9,6 +9,7 @@ public class UserController {
     @RequestMapping("login.do")
     public String login(){
         System.out.println("登录页面");
+        System.out.println("000");
         return "index";
     }
 
