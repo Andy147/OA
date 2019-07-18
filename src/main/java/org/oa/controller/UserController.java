159dccbc45aa8376fb2f5174ee06d248c0d73095
andy <wlg118325338@163.com>
2019-07-18 08:42:12 +0800

    修改了登录

--- src/main/java/org/oa/controller/UserController.java
+++ src/main/java/org/oa/controller/UserController.java
@@ -1,15 +1,47 @@
 package org.oa.controller;
 
+import net.sf.json.JSONObject;
+import org.oa.model.User;
+import org.oa.service.IUserServer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    @RequestMapping("login.do")
-    public String login(){
+    @Autowired
+    HttpServletRequest request;
+    @Autowired
+    IUserServer userServer;
+    @Autowired
+    HttpSession session;
+    @RequestMapping("login.do" )
+    public void login(){
+        JSONObject reply = new JSONObject();
         System.out.println("登录页面");
-        System.out.println("000");
+        System.out.println("name:"+request.getParameter("name"));
+        //获取用户名和密码
+        User user = userServer.getUserByName(request.getParameter("name") , request.getParameter("pwd") );
+        if (user != null)
+        {
+            reply.put("statu" , 0);
+            reply.put("reason" , "OK");
+            session.setAttribute("user" , user);
+        }
+        else
+        {
+            reply.put("statu" , -1);
+            reply.put("reason" , "用户名或密码不存在");
+        }
+        System.out.println(reply.toString(2));
+    }
+    @RequestMapping("index.do")
+    public String turn2Index()
+    {
         return "index";
     }
 
